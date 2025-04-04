9ae8b17a6714da09cb9838b7ca63e39bf4c5a2d5
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-04-05 00:07:42 +0530

    Clean Web

--- app/index.jsx
+++
@@ -1,8 +0,0 @@
-import { Redirect } from "expo-router";
-import 'react-native-reanimated';
-import "../global.css"; 
-const Index = () => {
-  return <Redirect href="/home" />;
-};
-
-export default Index;
